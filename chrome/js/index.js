c97d1a67428ed704198d29831cf45f38b73ac02d
Kwabang <kwabang2827@gmail.com>
2022-10-03 01:31:21 +0900

    Support manifest v3

---
+++ chrome/js/index.js
@@ -0,0 +1,21 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeNetRequest.updateDynamicRules({
+    addRules: [{
+      id: 1001,
+      priority: 1,
+      action: {
+        type: 'redirect',
+        redirect: {
+          regexSubstitution: 'https://api.twitch.tyo.kwabang.net/hls-raw/\\1',
+        }
+      },
+      condition: {
+        regexFilter: '^https://usher.ttvnw.net/api/channel/hls/(.*)',
+        resourceTypes: [
+          'xmlhttprequest'
+        ]
+      }
+    }],
+    removeRuleIds: [1001]
+  })
+})
